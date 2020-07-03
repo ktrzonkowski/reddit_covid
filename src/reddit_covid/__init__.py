7cc3015d0fac2630e05e0e0ec52a675769022dbb
Fulton Byrne <fulton.byrne@commercetools.de>
2020-07-03 18:09:00 -0400

    post graph to subreddit

--- src/reddit_covid/__init__.py
+++ src/reddit_covid/__init__.py
@@ -3,11 +3,13 @@ __version__ = "0.1.0"
 from . import fetch
 from . import present
 from . import constants
+from . import post
 
 
 def main():
 
     for state in constants.stateConfig.keys():
         df = fetch.fetch(state)
-        present.build_image(present.graph(df), constants.stateConfig[state])
-        post(constants.stateConfig[state])
+        stateConf = constants.stateConfig[state]
+        present.build_image(present.graph(df), stateConf)
+        post.post(stateConf)
--- src/reddit_covid/post.py
+++ src/reddit_covid/post.py
@@ -6,9 +6,9 @@ from . import praw_config
 def post(stateConfig):
     reddit = praw.Reddit(**praw_config.prawConf)
 
-    for subreddit in stateConfig.subreddits:
+    for subreddit in stateConfig['subreddits']:
         sr = reddit.subreddit(subreddit)
         sr.submit_image(
             title=f"Testing: {stateConfig['name']} COVID-19 Daily New Positive",
-            image_path=f"./fig-{stateConfig['name'].replace(' ', '_')}"
+            image_path=f"./fig-{stateConfig['name'].replace(' ', '_')}.png"
         )
